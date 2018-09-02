ec583aaded7c9c76fedc99cda4c64755f63aaab3
smilecamo <smilecamo99@gmail.com>
2018-09-02 23:03:24 +0800

    like组件

---
+++ components/like/index.js
@@ -0,0 +1,29 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    like: false,
+    count1: 99,
+    count2: 999,
+    yesSrc: './images/like.png',
+    noSrc: './images/like@dis.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike:function(event) {
+      console.log(event)
+    }
+  }
+})
