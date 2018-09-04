3ace1ca3a5990a899b03287572107e84c60708f8
smilecamo <smilecamo99@gmail.com>
2018-09-04 22:46:24 +0800

    promise

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -8,7 +8,9 @@ Component({
   behaviors: [classicBeh],
   properties: {
   },
-
+  detached:function() {
+    wxAm.pause()
+  },
   /**
    * 组件的初始数据
    */
@@ -24,14 +26,14 @@ Component({
    */
   methods: {
     play:function() {
-      if(!this.data.playing){
+      if(this.data.playing){
         this.setData({
-          playing: true
+          playing: false
         })
         wxAm.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
       }else{
         this.setData({
-          playing: false
+          playing: true
         })
         wxAm.pause()
       }
